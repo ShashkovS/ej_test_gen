d388a483de20670121f2c6d33db779e3ec70af88
Sergey Shashkov <sh57@yandex.ru>
2024-08-30 12:13:00 +0300

    feat: upd

--- src/ej_test_gen/__about__.py
+++ src/ej_test_gen/__about__.py
@@ -5,7 +5,7 @@ __all__ = [
     "__copyright__",
 ]
 
-__version__ = '0.1.4'
+__version__ = '0.1.5'
 __author__ = "Sergey Shashkov"
 __license__ = "MIT"
 __copyright__ = "Copyright 2019- Sergey Shashkov"
